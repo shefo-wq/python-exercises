e77f2a52cbf7d1c5f6862449ebcf862f5badfeef
shefo-wq <shefoloss55@gmail.com>
2026-03-30 21:42:15 +0200

    first commit

--- scope.py
+++ scope.py
@@ -1,20 +1,16 @@
-x=10
-
+x = 10
 
 def one():
     global x
-
-
-    x=2
+    x = 2
     print(f"print variable from global scope {x}")
 
 def two():
-    x=4
-    print(f"print variable from global scope {x}")    
+    x = 4
+    print(f"print variable from global scope {x}")
 
 print(f"print variable from function scope {x}")
 one()
-
 two()
 print(f"print variable from function scope {x}")
-print(f"print variable from global  scope after one function is called {x}")
+print(f"print variable from global scope after one function is called {x}")
