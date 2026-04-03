d8327d12a19823f92f7e1f19b1788d6ff71933b7
shefo-wq <shefoloss55@gmail.com>
2026-04-03 23:05:11 +0200

    add cleanword exercise

---
+++ cleanword.py
@@ -0,0 +1,8 @@
+def cleanword(word):
+    if len(word) == 1:
+        return word
+    if word[0] == word[1]:
+        return cleanword(word[1:])
+    return word[0] + cleanword(word[1:])
+
+print(cleanword("ssshhefffoo"))
--- scope.py
+++ scope.py
@@ -1,16 +1,8 @@
-x = 10
+def cleanword(word):
 
-def one():
-    global x
-    x = 2
-    print(f"print variable from global scope {x}")
-
-def two():
-    x = 4
-    print(f"print variable from global scope {x}")
-
-print(f"print variable from function scope {x}")
-one()
-two()
-print(f"print variable from function scope {x}")
-print(f"print variable from global scope after one function is called {x}")
+    if len(word)==1:
+        return word
+    if word[0]==word[1]  :
+        return cleanword(word[1:])  
+    return word[0]+cleanword(word[1:])  
+print(cleanword("ssshhefffoo"))        
